06c84d24702ce47678ce48ee993598015a3aeb4f
Kellieys <y10a.kelly@example.com>
2020-12-22 02:56:53 +0800

    mainactivity general idea

---
+++ app/src/main/java/com/example/runningtracker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.runningtracker;
+
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
