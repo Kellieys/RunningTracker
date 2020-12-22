8ef02a44b2cf93660ddeacb01512b6aaeebaa8af
Kellieys <y10a.kelly@example.com>
2020-12-23 02:04:34 +0800

    update

--- app/src/main/java/com/example/runningtracker/MainActivity.java
+++ app/src/main/java/com/example/runningtracker/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.runningtracker;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.widget.Button;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Local variable
+        Button startTimer_button = findViewById(R.id.startTimer_button);
+        Button runHistory_button = findViewById(R.id.runHistory_button);
     }
-}
+
+    // Timer button clicked function
+    public void timer_onClick(View view)
+    {
+        Intent intent = new Intent(this, TimerActivity.class);
+        Intent serviceIntent = new Intent(this, ServiceActivity.class);
+
+        // start activity and start service
+        startActivity(intent);
+        startService(serviceIntent);
+    }
+
+    // go to sorting activity
+    public void runHistory_OnClick(View view)
+    {
+        Intent intent = new Intent(this, SortActivity.class);
+        startActivity(intent);
+    }
+
+} // end class
---
+++ app/src/main/java/com/example/runningtracker/SortActivity.java
@@ -0,0 +1,23 @@
+package com.example.runningtracker;
+
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SortActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_start_logging);
+//        updateTimeTextView = findViewById(R.id.updateTimeTextView);
+//        updateDistanceTextView = findViewById(R.id.updateDistanceTextView);
+//        stopLoggingButton = findViewById(R.id.stopLoggingButton);
+    }
+
+
+
+} // end class
